9d7e473c97f4e5c393a569833291d49271f1e563
satyam01mirgane <satyammirgane01@gmail.com>
2025-01-07 00:28:07 +0530

    final

--- resources/views/front/common/profile-header.blade.php
+++ resources/views/front/common/profile-header.blade.php
@@ -19,16 +19,11 @@
   <!-- Navbar -->
   <nav class="main-header navbar navbar-expand navbar-white navbar-light">
     <!-- Left navbar links -->
-    <ul class="navbar-nav">
-      <li class="nav-item">
-        <a class="nav-link" href="#" role="button"></a>
-      </li>     
-    </ul>
-
+   
     <!-- Right navbar links -->
-    <ul class="navbar-nav ml-auto">
+   >
       <!-- Navbar Search -->
-      <li class="nav-item">
+   
         <!--<a class="nav-link" data-widget="navbar-search" href="#" role="button">
           <i class="fas fa-search"></i>
         </a>
