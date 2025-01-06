42b92927eee66990995b909591fa26ee569dd66e
satyam01mirgane <satyammirgane01@gmail.com>
2025-01-07 00:04:36 +0530

    final

--- resources/views/front/common/sidebar.blade.php
+++ resources/views/front/common/sidebar.blade.php
@@ -1,46 +1,115 @@
-<nav class="main-header navbar navbar-expand-md navbar-dark bg-dark">
-    <div class="container-fluid">
-        <a href="{{url('/')}}" class="navbar-brand">
-            <img src="{{asset('assets/images/logo.svg')}}" alt="Logo" class="brand-image" style="height: 40px;">
-        </a>
+<!-- Main Sidebar Container -->
+<aside class="main-sidebar sidebar-dark-primary elevation-4">
+    <!-- Brand Logo -->
+    <a href="{{url('/')}}" class="brand-link">
+      <img src="{{asset('assets/images/logo.svg')}}">
+      <span></span>
+    </a>
 
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse">
+    <!-- Sidebar -->
+    <div class="sidebar">
+
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse">
             <span class="navbar-toggler-icon"></span>
         </button>
-
-        <div class="collapse navbar-collapse" id="navbarCollapse">
-            <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
-                <li class="nav-item">
-                    <a href="{{url('dashboard')}}" class="nav-link {{$menu1}}">
-                        <i class="nav-icon fas fa-tachometer-alt"></i> Dashboard
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a href="{{url('scheduled-course')}}" class="nav-link {{$menu7}}">
-                        <i class="nav-icon far fa-image"></i> Scheduled Courses
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a href="{{url('certificate-feedback')}}" class="nav-link {{$menu5}}">
-                        <i class="nav-icon fas fa-tachometer-alt"></i> Certificate & Feedback
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a href="{{url('change-password')}}" class="nav-link {{$menu2}}">
-                        <i class="nav-icon far fa-calendar-alt"></i> Change Password
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a href="{{url('logout')}}" class="nav-link">
-                        <i class="nav-icon fas fa-sign-out-alt"></i> Logout
-                    </a>
-                </li>
-            </ul>
-        </div>
+      <!-- Sidebar user (optional) -->
+    
+      <!-- SidebarSearch Form -->
+      <!-- Sidebar Menu -->
+      <nav class="mt-2">
+        <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+          <!-- Add icons to the links using the .nav-icon class
+               with font-awesome or any other icon font library -->
+          <li class="nav-item">
+            <a href="{{url('dashboard')}}" class="nav-link {{$menu1}}">
+              <i class="nav-icon fas fa-tachometer-alt"></i>
+              <p>
+                Dashboard
+              </p>
+            </a>
+          </li>       
+          <!-- <li class="nav-item">
+            <a href="{{url('enrolled-workshop')}}" class="nav-link {{$menu3}}">
+              <i class="nav-icon far fa-image"></i>
+              <p>
+                Enrolled Workshops
+              </p>
+            </a>
+          </li>
+           
+		  <li class="nav-item">
+            <a href="{{url('scheduled-workshop')}}" class="nav-link {{$menu4}}">
+              <i class="nav-icon far fa-calendar-alt"></i>
+              <p>
+                Scheduled Workshops
+              </p>
+            </a>
+          </li>
+		   -->
+		  <!-- <li class="nav-item">
+            <a href="{{url('enrolled-course')}}" class="nav-link {{$menu6}}">
+              <i class="nav-icon far fa-image"></i>
+              <p>
+               Enrolled Courses
+              </p>
+            </a>
+          </li> -->
+		  
+		  <li class="nav-item">
+            <a href="{{url('scheduled-course')}}" class="nav-link {{$menu7}}">
+              <i class="nav-icon far fa-image"></i>
+              <p>
+                Scheduled Courses
+              </p>
+            </a>
+          </li>
+		  
+		  <li class="nav-item">
+            <a href="{{url('certificate-feedback')}}" class="nav-link {{$menu5}}">
+              <i class="nav-icon fas fa-tachometer-alt"></i>
+              <p>
+                Certificate & Feedback
+              </p>
+            </a>
+          </li>
+		   <!-- <li class="nav-item">
+            <a href="{{url('quiz-list')}}" class="nav-link {{$menu8}}">
+              <i class="nav-icon fas fa-question"></i>
+              <p>
+                Online Quiz
+              </p>
+            </a>
+          </li> -->
+		   <!-- <li class="nav-item">
+            <a href="{{url('test-list')}}" class="nav-link {{$menu9}}">
+              <i class="nav-icon fas fa-question"></i>
+              <p>
+                Online Test
+              </p>
+            </a>
+          </li> -->
+          <li class="nav-item">
+            <a href="{{url('change-password')}}" class="nav-link {{$menu2}}">
+              <i class="nav-icon far fa-calendar-alt"></i>
+              <p>
+                Change Password
+              </p>
+            </a>
+          </li>
+            <li class="nav-item">
+            <a href="{{url('logout')}}" class="nav-link">
+              <i class="nav-icon fas fa-sign-out-alt"></i>
+              <p>
+                Logout
+              </p>
+            </a>
+          </li>         
+        </ul>
+      </nav>
+      <!-- /.sidebar-menu -->
     </div>
-</nav>
-
-<style>
+    <!-- /.sidebar -->
+    <style>
 .navbar-collapse {
     transition: all 0s ease;
 }
@@ -87,3 +156,9 @@ $(document).ready(function() {
     });
 });
 </script>
+
+  </aside>
+
+
+
+
