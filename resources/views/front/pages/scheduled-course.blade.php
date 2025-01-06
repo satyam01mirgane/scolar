d62344e8b57988bdc96edc9e75e5135737daca43
satyam01mirgane <satyammirgane01@gmail.com>
2025-01-06 22:14:09 +0530

    final

--- resources/views/front/pages/scheduled-course.blade.php
+++ resources/views/front/pages/scheduled-course.blade.php
@@ -1,18 +1,16 @@
 @include('front.common.sidebar')
-<!-- @include('front.common.profile-header') -->
-<!-- Content Wrapper. Contains page content -->
   <div class="content-wrapper">
     <!-- Content Header (Page header) -->
     <section class="content-header">
       <div class="container-fluid">
         <div class="row mb-2">
           <div class="col-sm-6">
-            <h1>Scheduled  Masterclass</h1>
+            <h1>Enrolled Courses</h1>
           </div>
           <div class="col-sm-6">
             <ol class="breadcrumb float-sm-right">
               <li class="breadcrumb-item"><a href="{{url('/')}}">Home</a></li>
-              <li class="breadcrumb-item active">Scheduled  Masterclass</li>
+              <li class="breadcrumb-item active">Enrolled Courses</li>
             </ol>
           </div>
         </div>
@@ -23,9 +21,7 @@
     <section class="content">
       <div class="container-fluid">
         <div class="row">         
-          <!-- /.col -->
           <div class="col-md-12">
-            
             <div class="card">
               <div class="card-body table-responsive p-0">
                 <table class="table table-hover text-nowrap">
@@ -33,52 +29,18 @@
                     <tr>
                       <th>Name</th>
                       <th>Zoom Link</th>
-                      <th> Masterclass ID</th>
+                      <th>Course ID</th>
                       <th>Instructor</th>
                       <th>Date</th>
-					  <th>Time</th>
-					  <th>Amount Paid</th>
-                      <!-- <th>Invoice</th> -->
-                    
-					  <th>Status</th>
-				
+                      <th>Time</th>
+                      <th>Amount Paid</th>
+                      <th>Status</th>
                     </tr>
                   </thead>
                   <tbody>
-					@if(count($orders)>0)
-					@foreach($orders as $k=>$v)
-                    <tr>
-                      <td>{{$v->workshopname}}</td>
-                      <td><a href="{{$v->zoom_link}}">Link</a></td>
-                      <td>WKPID{{$v->product_id}}</td>
-                      <td>{{$v->trainername}}</td>
-                      <td>{{date('d-m-Y',strtotime($v->session_date))}}</td>
-					  <td>{{date('H:i:s A',strtotime($v->session_time))}}</td>
-                      <td>@if($v->workshop_type=='Free') Free @else Rs.{{$v->price - $v->product_discount}} @endif</td>
-                      <!-- <td><a href="#">@if($v->workshop_type=='Free') Free @else Invoice @endif</a></td> -->
-                    
-					  <td>{{$v->session_status}}</td>
-                    </tr>
-					@endforeach
-					@else
-						<tr>
-							<td colspan="9" align="center">No  Masterclass found</td>
-						</tr>
-					@endif
-                  </tbody>
-                </table>
-              </div>
-              <!-- /.card-body -->
-            </div>
-            <!-- /.card -->
-          </div>
-          <!-- /.col -->
-        </div>
-        <!-- /.row -->
-      </div><!-- /.container-fluid -->
-    </section>
-    <!-- /.content -->
-    
-  </div>
-  <!-- /.content-wrapper -->
-@include('front.common.profile-footer')
+                    @if(count($orders) > 0)
+                      @foreach($orders as $v)
+                        <tr>
+                          <td>{{$v->workshopname}}</td>
+                          <td><a href="{{$v->zoom_link}}">Link</a></td>
+                          <td>WKPID{{$v->product_id
--- resources/views/front/pages/user-account.blade.php
+++ resources/views/front/pages/user-account.blade.php
@@ -1,4 +1,3 @@
-<!-- @include('front.common.profile-header') -->
 @include('front.common.sidebar')
 <!-- Content Wrapper. Contains page content -->
   <div class="content-wrapper">
@@ -24,14 +23,14 @@
       <div class="container-fluid">
         <div class="row">
           <div class="col-md-3">
-			@php
-			if(isset(Auth::user()->photo))
-			{
-				$photo = asset('profile/'.Auth::user()->photo);
-			}else{
-				$photo = asset('user.png');
-			}
-			@endphp
+            @php
+            if(isset(Auth::user()->photo))
+            {
+                $photo = asset('profile/'.Auth::user()->photo);
+            }else{
+                $photo = asset('user.png');
+            }
+            @endphp
             <!-- Profile Image -->
             <div class="card card-primary card-outline">
               <div class="card-body box-profile">
@@ -43,103 +42,61 @@
 
                 <h3 class="profile-username text-center">{{Auth::user()->name}}</h3>
                 <div class="text-center text-sm text-primary">Member Since {{date('d M Y',strtotime(Auth::user()->created_at))}}</div>
-                
-
-              </div>
-              <!-- /.card-body -->
-            </div>
-            <!-- /.card -->
-
-            <!-- About Me Box -->
-            <div class="card card-primary">
-              <div class="card-header">
-                <h3 class="card-title">About Me</h3>
-              </div>
-              <!-- /.card-header -->
-              <div class="card-body">
-                
-                <strong><i class="fas fa-map-marker-alt mr-1"></i> Location</strong>
-
-                <p class="text-muted">{{Auth::user()->address}}</p>
               </div>
               <!-- /.card-body -->
             </div>
             <!-- /.card -->
           </div>
-          <!-- /.col -->
           <div class="col-md-9">
             <div class="card">
               <div class="card-body p-0">
-				@if ($message = Session::get('success'))
-				  <div class="alert alert-success">
-					  <p class="text-green-800">{{ $message }}</p>
-				  </div>
-				@endif
+                @if ($message = Session::get('success'))
+                  <div class="alert alert-success">
+                      <p class="text-green-800">{{ $message }}</p>
+                  </div>
+                @endif
                 <table class="table">
-				<td><a href="{{url('profile-edit')}}" class="badge bg-danger">Edit</a>
+                  <td><a href="{{url('profile-edit')}}" class="badge bg-danger">Edit</a>
                   <tbody>
-				  
                     <tr>
-                      <th>Personl ID</th>
+                      <th>Personal ID</th>
                       <td>User{{Auth::user()->unique_id}}</td>
-                      
                     </tr>
                     <tr>
-                      <th> Name</th>
-                      <td>{{Auth::user()->name}} </td>
-                     
+                      <th>Name</th>
+                      <td>{{Auth::user()->name}}</td>
                     </tr>
-					<tr>
-                      <th> Email Address</th>
+                    <tr>
+                      <th>Email Address</th>
                       <td>{{Auth::user()->email}}</td>
-                 
                     </tr>
                     <tr>
                       <th>Contact Number</th>
                       <td>{{Auth::user()->phone_number}}</td>
-                 
                     </tr>
-					<tr>
+                    <tr>
                       <th>Password</th>
                       <td>**************</td>
-                     
                     </tr>
-                    
-                    
                     <tr>
                       <th>Address</th>
                       <td>{{Auth::user()->address}}</td>
-                    
                     </tr>
                     <tr>
                       <th>Pincode</th>
                       <td>{{Auth::user()->pincode}}</td>
                     </tr>
-					
-					<tr>
+                    <tr>
                       <th>Date of Birth</th>
-                      <td>{{date('d-M-Y',strtotime(Auth::user()->dob))}}
+                      <td>{{date('d-M-Y',strtotime(Auth::user()->dob))}}</td>
                     </tr>
-					
                   </tbody>
-              </td>  </table>
+                </table>
               </div>
-              <!-- /.card-body -->
             </div>
-            <!-- /.card -->
-
-            </div>
-            <!-- /.card -->
           </div>
-          <!-- /.col -->
         </div>
-        <!-- /.row -->
       </div><!-- /.container-fluid -->
     </section>
-    <!-- /.content -->
   </div>
-  <!-- /.content-wrapper -->
 @include('front.common.profile-footer')
-<script>
-setTimeout(function() {$(".alert-success").slideToggle();}, 2000);
-</script>
