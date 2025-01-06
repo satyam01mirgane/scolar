c195b4ca82260404f00a85c11cbbdd5c86351102
satyam01mirgane <satyammirgane01@gmail.com>
2025-01-07 00:33:04 +0530

    final

--- resources/views/front/pages/scheduled-course.blade.php
+++ resources/views/front/pages/scheduled-course.blade.php
@@ -3,7 +3,7 @@
 <!-- Content Wrapper. Contains page content -->
   <div class="content-wrapper">
     <!-- Content Header (Page header) -->
-    <section class="content-header">
+    <section class="content">
       <div class="container-fluid">
         <div class="row mb-2">
           <div class="col-sm-6">
