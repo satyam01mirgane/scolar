48ecc8501e5ba666e01504c6cab574e85f39a94a
satyam01mirgane <satyammirgane01@gmail.com>
2025-01-06 18:33:23 +0530

    bdb

--- resources/views/front/pages/user-account.blade.php
+++ resources/views/front/pages/user-account.blade.php
@@ -1,5 +1,5 @@
 <!-- @include('front.common.profile-header') -->
-@include('front.common.slider')
+@include('front.common.sidebar')
 <!-- Content Wrapper. Contains page content -->
 <div class="content-wrapper" style="background-color: #f4f6f9; min-height: 100vh; padding: 20px;">
     <!-- Content Header (Page header) -->
