762e9221523eb1a37ef075302a911210a97380b6
satyam01mirgane <satyammirgane01@gmail.com>
2025-01-12 16:59:28 +0530

    final

--- resources/views/front/pages/certificate-feedback.blade.php
+++ resources/views/front/pages/certificate-feedback.blade.php
@@ -1,11 +1,12 @@
 @include('front.common.profile-header')
 @include('front.common.sidebar')
-<link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" rel="stylesheet">
 
 <style>
 .modal-header1 {
-    display: flex;
+    display: -ms-flexbox;
+    -ms-flex-align: start;
     align-items: flex-start;
+    -ms-flex-pack: justify;
     justify-content: space-between;
     padding: 1rem;
     border-bottom: 1px solid #e9ecef;
@@ -14,9 +15,9 @@
 }
 </style>
 
-<!-- Content Wrapper -->
-<div class="content-wrapper" style="margin-left:0px;">
-    <!-- Content Header -->
+<!-- Content Wrapper. Contains page content -->
+<div class="content-wrapper" style="margin-left:0px;>
+    <!-- Content Header (Page header) -->
     <section class="content-header">
         <div class="container-fluid">
             <div class="row mb-2">
@@ -25,25 +26,26 @@
                 </div>
                 <div class="col-sm-6">
                     <ol class="breadcrumb float-sm-right">
-                        <li class="breadcrumb-item"><a href="{{ url('/') }}">Home</a></li>
+                        <li class="breadcrumb-item"><a href="{{url('/')}}">Home</a></li>
                         <li class="breadcrumb-item active">Certificate & Feedback</li>
                     </ol>
                 </div>
             </div>
-        </div>
+        </div><!-- /.container-fluid -->
     </section>
 
     <!-- Main content -->
     <section class="content">
         <div class="container-fluid">
             <div class="row">
+                <!-- /.col -->
                 <div class="col-md-12">
                     <div class="card">
                         <div class="card-body table-responsive p-0">
                             @if(Session::has('success'))
-                                <div class="alert alert-success">
-                                    {{ Session::get('success') }}
-                                </div>
+                            <div class="alert alert-success">
+                                {{ Session::get('success') }}
+                            </div>
                             @endif
                             <table class="table table-hover text-nowrap">
                                 <thead>
@@ -53,50 +55,37 @@
                                         <th>Date</th>
                                         <th>Time</th>
                                         <th>Certificate</th>
-                                        <th>Share</th>
+                                        <!-- <th>Feedback</th> -->
                                     </tr>
                                 </thead>
                                 <tbody>
-                                    @if(count($orders) > 0)
-                                        @foreach($orders as $v)
-                                            <tr>
-                                                <td>{{ $v->workshopname }}</td>
-                                                <td>WKPID{{ $v->product_id }}</td>
-                                                <td>{{ date('d-m-Y', strtotime($v->session_date)) }}</td>
-                                                <td>{{ date('H:i:s A', strtotime($v->session_time)) }}</td>
-                                                <td>
-                                                    @if($v->session_status != 'Open')
-                                                        <a href="{{ url('certificates/'.$v->product_id.'.pdf') }}" target="_blank">Certificate</a>
-                                                    @else
-                                                        <span style="color:red;">Certificate not completed.</span>
-                                                    @endif
-                                                </td>
-                                                <td>
-                                                    @if($v->session_status != 'Open')
-                                                        <div class="d-flex justify-content-start gap-3">
-                                                            <!-- LinkedIn Share -->
-                                                            <a href="https://www.linkedin.com/sharing/share-offsite/?text=🎉 I’m thrilled to announce that I’ve successfully completed the masterclass '{{ $v->workshopname }}' under the expert guidance of {{ $v->trainername }}. 🚀💡 This experience has been truly enriching! 
-                                                                #VIEF #Vscholar @vscholar #{{ str_replace(' ', '', $v->workshopname) }} #{{ str_replace(' ', '', $v->trainername) }}"
-                                                               target="_blank" title="Share on LinkedIn">
-                                                                <i class="fab fa-linkedin" style="color: #0077b5; font-size: 24px;"></i>
-                                                            </a>
-
-                                                            <!-- Copy to Clipboard -->
-                                                            <a href="#" onclick="copyToClipboard('🎉 I’m thrilled to announce that I’ve successfully completed the masterclass \'{{ $v->workshopname }}\' under the expert guidance of {{ $v->trainername }}. 🚀💡 This experience has been truly enriching! 
-                                                                #VIEF #Vscholar @vscholar #{{ str_replace(' ', '', $v->workshopname) }} #{{ str_replace(' ', '', $v->trainername) }}')" title="Copy Link">
-                                                                <i class="fas fa-copy" style="color: #6c757d; font-size: 24px;"></i>
-                                                            </a>
-                                                        </div>
-                                                    @else
-                                                        <span style="color:red;">Not available</span>
-                                                    @endif
-                                                </td>
-                                            </tr>
-                                        @endforeach
+                                    @if(count($orders)>0)
+                                    @foreach($orders as $k=>$v)
+                                    <tr>
+                                        <td>{{ $v->workshopname }}</td>
+                                        <td>WKPID{{ $v->product_id }}</td>
+                                        <td>{{ date('d-m-Y', strtotime($v->session_date)) }}</td>
+                                        <td>{{ date('H:i:s A', strtotime($v->session_time)) }}</td>
+                                        <td>
+                                            @if($v->session_status != 'Open')
+                                            <a href="{{ url('print-certificate/'.$v->product_id) }}">Certificate</a>
+                                            @else 
+                                            <span style="color:red;">Certificate not completed.</span>
+                                            @endif
+                                        </td>
+                                        <td>
+                                            <!-- @if(!empty($v->feedback))
+                                            <span>Feedback Submitted</span>
+                                            @else
+                                            <a href="#" data-toggle="modal" data-target="#myModal" onclick="feedback('{{ $v->product_id }}')">Feedback</a>
+                                            @endif -->
+                                        </td>
+                                    </tr>
+                                    @endforeach
                                     @else
-                                        <tr>
-                                            <td colspan="6" align="center">No workshop found</td>
-                                        </tr>
+                                    <tr>
+                                        <td colspan="6" align="center">No workshop found</td>
+                                    </tr>
                                     @endif
                                 </tbody>
                             </table>
@@ -104,17 +93,64 @@
                     </div>
                 </div>
             </div>
-        </div>
-    </section>
-</div>
+        </div><!-- /.container-fluid -->
+    </section><!-- /.content -->
+</div><!-- /.content-wrapper -->
 
-<script>
-    // Copy to Clipboard Functionality
-    function copyToClipboard(text) {
-        navigator.clipboard.writeText(text).then(function() {
-            alert("Message copied to clipboard!");
-        }, function(err) {
-            console.error("Failed to copy: ", err);
-        });
-    }
-</script>
+<!-- Modal -->
+<div id="myModal" class="modal fade" role="dialog">
+    <div class="modal-dialog modal-lg">
+        <!-- Modal content-->
+        <div class="modal-content">
+            <div class="modal-header1">
+                <button type="button" class="close" data-dismiss="modal">&times;</button>
+                <h4 class="modal-title">Feedback</h4>
+            </div>
+            <div class="modal-body">
+                <form id="contact_form" name="contact_form" class="default-form" method="post" action="{{ url('feedback') }}">
+                    @csrf
+                    <input type="hidden" name="wpid" id="wpid" value="">
+                    <div class="row">
+                        @if(Session::has('success'))
+                        <div class="alert alert-success">
+                            {{ Session::get('success') }}
+                        </div>
+                        @endif
+                        <div class="col-md-12">
+                            <div class="form-group">
+                                <label class="form-label">Email address</label>
+                                <input class="form-control" type="email" name="email" placeholder="Your Answer" required="">
+                            </div>
+                            <div class="form-group">
+                                <label class="form-label">Name</label>
+                                <input class="form-control" type="text" name="name" placeholder="Your Answer" required="">
+                            </div>
+                        </div>
+                        <div class="col-md-12">
+                            <div class="form-group">
+                                <label class="form-label">Contact No.</label>
+                                <input class="form-control" type="text" name="phone" placeholder="Your Answer" required="">
+                            </div>
+                            <div class="form-group">
+                                <label class="form-label">Please Confirm Your name for the Certificates</label>
+                                <input class="form-control" type="text" name="cname" placeholder="Your Answer" required="">
+                            </div>
+                        </div>
+                        <div class="col-md-12">
+                            <div class="form-group">
+                                <label class="form-label">Please rate the overall workshop/course<span>*</span></label>
+                                <div class="form-check form-check-inline">
+                                    <input class="form-check-input" type="radio" name="workshop_rate" id="inlineRadio1" value="1">
+                                    <label class="form-check-label" for="inlineRadio1">1</label>
+                                </div>
+                                <!-- Add other rating options here -->
+                            </div>
+                            <!-- Add other form fields for feedback -->
+                            <button type="submit" class="btn btn-primary">Submit Feedback</button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
